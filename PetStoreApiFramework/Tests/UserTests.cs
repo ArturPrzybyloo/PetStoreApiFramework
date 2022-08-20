c579bb17a3b7abb191aff51a0742f8bf17a960bb
Artur Przybyło <a.przybyloo@gmail.com>
2022-08-20 10:39:57 +0200

    Added method for replacing variables from default json files, added some test for store/inventory

---
+++ PetStoreApiFramework/Tests/UserTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using NUnit.Allure.Attributes;
+using NUnit.Allure.Core;
+using NUnit.Framework;
+using PetStoreApiFramework.Requests.Store;
+using PetStoreApiFramework.Utils;
+using PetStoreApiFramework.Utils.User;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+
+namespace PetStoreApiFramework.Tests
+{
+    [AllureNUnit]
+    [AllureSuite("Pet Tests")]
+    [Category("User Tests")]
+    public class UserTests : TestBase
+    {
+        UserObject user = new UserObject();
+
+        [TestCase]
+        [Order(1)]
+        public void CreateUsersWithArray()
+        {
+            user.GetDeafult().Create();
+        }
+    }
+}
